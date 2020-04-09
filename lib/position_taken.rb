dffe3ef8c9c903ffd236e8fbde38c74317307534
Andrew C <acchang4@gmail.com>
2020-04-09 02:46:11 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -5,6 +5,8 @@ def position_taken?(board,index)
     false
   elsif (board[index]) == nil
     false
+  elsif (board[index]) == X || O
+    true
   end
 end
 # code your #position_taken? method here!
